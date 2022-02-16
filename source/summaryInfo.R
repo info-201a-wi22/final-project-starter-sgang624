ed30c2f7aeb0ddc535206ae62bd87af3d645ad53
vmulugeta1 <vmulu1@uw.edu>
2022-02-16 14:37:09 -0800

    Summary Info First Iteration

---
+++ source/summaryInfo.R
@@ -0,0 +1,23 @@
+source("source/cleanData.R")
+summary_info <- list()
+summary_info$num_observations <- nrow(hpi_crime_clean)
+summary_info$num_features <- ncol(hpi_crime_clean)
+summary_info$min_hpi_value <- hpi_crime_clean %>% 
+  pull(index_nsa) %>% 
+  min(na.rm = TRUE)
+summary_info$max_hpi_value <- hpi_crime_clean %>% 
+  pull(index_nsa) %>% 
+  max(na.rm = TRUE)
+summary_info$mean_hpi_value <- hpi_crime_clean %>% 
+  pull(index_nsa) %>% 
+  mean(na.rm = TRUE)
+summary_info$some_max_value <- my_dataframe %>%
+  filter(some_var == max(some_var, na.rm = T)) %>%
+  select(some_label)
+summary_info$num_city_state <- hpi_crime_clean %>% 
+  select(City..State) %>% 
+  distinct() %>% 
+  count() %>% 
+  pull()
+summary_info$major_crime <- c("Homicides", "Rapes", "Assaults", "Robberies")
+  
