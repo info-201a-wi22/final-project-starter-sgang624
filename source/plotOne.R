53a11279e04c5f9ee347f37007caa62fed965e46
MikeRamsay <mramsay2@uw.edu>
2022-02-16 17:54:55 -0800

    plotOne third iteration

--- source/plotOne.R
+++ source/plotOne.R
@@ -1,15 +1,15 @@
 source("source/cleanData.R")
 
 plotOnedata <- hpi_crime_clean %>%
-  mutate(VCR_100k = (Violent.Crimes/Population)) 
+  mutate(VCR = (Violent.Crimes/Population)) 
 
 plotOne <- ggplot(data = plotOnedata) + 
-  geom_point(mapping = aes(x = index_nsa, y = VCR_100k)) +
-  geom_smooth(mapping = aes(x = index_nsa, y = VCR_100k)) +
+  geom_point(mapping = aes(x = index_nsa, y = VCR)) +
+  geom_smooth(mapping = aes(x = index_nsa, y = VCR)) +
   labs(
     title = "Housing Price Index vs Violent Crime Rate",
     x = "Housing Price Index Value",
-    y = "Violent Crime Rate (per 100,000 people)"
+    y = "Violent Crime Rate"
   )
 
 ggplotly(plotOne)
