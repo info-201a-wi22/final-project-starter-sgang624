7bddc5ca4a3fab27ce4025cf628fa7c9a3b3e8a2
MikeRamsay <mramsay2@uw.edu>
2022-03-07 16:00:54 -0800

    added files

--- source/app_ui.R
+++ source/app_ui.R
@@ -8,8 +8,8 @@ ui <- navbarPage(
   "Safe n' Sound",
   intro_page,
   interactive_page_one,
-  interactive_page_one,
-  interactive_page_one,
-  summary_takeawys_page, 
+  interactive_page_two,
+  interactive_page_three,
+  summary_takeaways_page, 
   report_page
 )
