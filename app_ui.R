53cc6c91a32a222731caf3086c23af8b94c05738
MikeRamsay <mramsay2@uw.edu>
2022-03-08 23:40:31 -0800

    Added Descriptions

--- app_ui.R
+++ app_ui.R
@@ -28,18 +28,14 @@ interactive_page_one <- tabPanel(
       ),
       mainPanel(
         plotlyOutput("plotOne"),
-        h2("Purpose"),
-        p("This is plot is a scatter plot that shows the relationship between HPI Values and The Violent Crime Rate associated with this values."),
-        h2("Insights"),
-        p("This plot was made to answer the research question of", tags$b("how safe are the neighborhoods 
-          in low-income area"),". It is clear from this plot, neighborhoods with HPI values higher 
-          than", em("200"), " experience way less crime compared to neighborhoods under", em("200"),". From 
-          HPI values between", em("200"), " to", em("100"),", there is a sharp increase in crime rate. From HPI values between ", em("100"), " to ", em("50"),", 
-          there is a sharp decrease in crime rate. From HPI values from ", em("50"), " to ", em("0"),", there is slight decrease in crime rate. 
-          From this plot, we conclude that less crime happens in high-income areas i.e., neighborhoods with", tags$b("HPI values higher than 200"), " 
-          and more crime happens in low-income area i.e., neighborhoods with", tags$b("HPI values less than 200"),". This mean low-income areas are much 
-          dangerous than high income areas. However, this is not true for Assualt Crimes. Compares to ", tags$b("HPI over 200"), ", Assualt Crimes Rate for",
-          tags$b("HPI under 50"), " are much lower compared to their high HPI counterpart.")
+        h2("Description"),
+        p("This scatter plot that shows the relationship between Housing Price 
+          Index (HPI) Values and various violent crimes associated with this values. The widget allows 
+          for the ability to select individaul crime types. This plot was made to answer the research 
+          question of how safe are the neighborhoods in low-income areas. It is clear from this plot, 
+          neighborhoods with HPI values higher than 200 experience way less crime compared to 
+          neighborhoods under that value. Robberies and rapes appear to be more associated with the 
+          lower HPI values.")
       )
     )
   )
@@ -63,22 +59,14 @@ interactive_page_two <- tabPanel(
     ),
     mainPanel(
       plotlyOutput("plotTwo"),
-      h2("Purpose"),
-      p("This plot displays assaults, homicides, rapes, and robberies as a 
+      h2("Description"),
+      p("This plot examines assaults, homicides, rapes, and robberies as a 
         percentage out of the total number of violent crimes for areas under 
-        a variety of HPI values. We thought that displaying it in 
-        the form of a stacked bar chart with a key was best for displaying 
-        multiple variables that make up a whole."),
-      h2("Insights"),
-      p("Looking at this plot, we can see that assaults and robberies make up 
-        the vast majority of total violent crimes for lower HPI values. 
-        Meanwhile, rapes and homicides make up significantly less of the total 
-        violent crimes. We were a little shocked by how small a proportion that 
-        homicides make out of the total proportion. So this visualization makes 
-        it pretty clear what types of violent crimes occur the most often in 
-        low-income areas. For areas HPI values over 200, assaults were the most 
-        crime to occur. However, areas with HPI values under 200, robberies were 
-        more prevalent.")
+        a variety of HPI values. The stacked bar chart allows for a clear display 
+        of the breakdown amoung the four listed crimes. The widget also allows for 
+        a deeper look at a few select HPI values. It appears that as HPI increases, 
+        assaults make up a larger proportion of violent crimes and robberies make up less.
+        Homicides and rapes don't appear to be that impacted.")
     )
   )
 )
@@ -99,20 +87,12 @@ interactive_page_three <- tabPanel(
     ),
     mainPanel(
       plotlyOutput("plotThree"),
-      h2("Purpose"),
-      p("The purpose of this plot is to answer the question regarding the difference 
-      between crime rates in low income and high income areas. The box plots are included 
-      because they delineates the spread of the data (percentage of crime) across each 
-        of the 3 categories: high, low, and medium"),
-      h2("Insights"),
-      p("There are no outlier in the high category, and it has an IQR of 0.74. 
-      There are several outliers in the low category, and it has an IQR of 0.94,
-      indicating it has a relatively high spread from the mean as opposed to other
-      categories. There are some outliers in the medium category,and it has an IQR 
-      of 0.63. Overall, the box plots show that low income areas have the highest 
-      percentage of crime, and the data extend beyond the whiskers of the plot (meaning 
-      that there are outliers). It's a fair assumption to make that low income 
-        areas have a higher percentage of crime as opposed to higher income areas.")
+      h2("Description"),
+      p("This chart attempts to answer the question regarding the difference 
+      between crime rates in low income and high income areas. The box plots 
+      clearly delineate the spread of the data (percentage of crime) across each 
+      of the 3 categories: high, low, and medium. The ability to select which one 
+      to display with the widget adds for further focus.")
     ) 
   )
 )
@@ -131,7 +111,14 @@ interactive_page_four <- tabPanel(
     ),
     mainPanel(
       plotlyOutput("plotFour"),
-      p("This plot looks at total violent crime numbers in the US, but it is split up displaying high and low income groups in separate graphs that can be controlled with the widget. We wanted this plot to display the differences in crime numbers across low income and high income areas, which it does a good job")
+      h2("Description"),
+      p("This plot looks at total violent crime numbers in the US, but it is 
+        split up displaying high and low income groups, by Housing Price Index 
+        (HPI), in separate graphs that can be controlled with the widget. We 
+        wanted this plot to display the differences in crime numbers across low 
+        income and high income areas, which it does a good job at. The plots 
+        show that there are more violent crimes for the lower HPI value and less
+        for the chart with the chart displaying the higher value.")
     ) 
   )
 )
@@ -140,7 +127,7 @@ interactive_page_four <- tabPanel(
 summary_takeaways_page <- tabPanel(
   "Summary Takeaways",
   titlePanel("Summary Takeaways"),
-  p("3 Summary paragraphs")
+  p("4 Summary takeaways")
 )
 
 report_page <- tabPanel(
